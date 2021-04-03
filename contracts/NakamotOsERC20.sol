00d57f1dcff85cca14ca903288d38aa6102a063a
Sam <hatem@clearrain.xyz>
2021-04-03 14:52:08 -0400

    feat: update contracts for lottery

--- contracts/NakamotOsERC20.sol
+++ contracts/NakamotOsERC20.sol
@@ -1,45 +1,76 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity 0.6.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "./NakamotOsERC721.sol";
 
-contract NakamotOsERC20 is ERC20 {
+contract NakamotOsERC20 is ERC20, VRFConsumerBase {
+    using SafeMathChainlink for uint256;
+
     event Burn(address indexed burner, uint256 amount);
+    event LotteryEntered(bytes32 indexed requestId, address indexed participant);
+    event LotteryResult(bytes32 indexed requestId, address indexed participant, bool winner);
 
-    mapping(address => uint256) public nftClaims;
-    address public nftTokenAddress;
+    NakamotOsERC721 public nftToken;
 
-    uint256 private DECIMAL_MULTIPLIER = 10 ** 18;
+    bytes32 private s_keyHash;
+    uint256 private s_fee;
+
+    mapping(bytes32 => address) private burner;
+
+    /**
+     * Technically there can be more than 210
+     */
+    uint256 public MAX_NFT_SUPPLY = 210;
+
+    uint256 public ONE = 10 ** 18;
 
     constructor(
         string memory name_,
         string memory symbol_,
         uint256 maxSupply,
         address bagHolder,
-        address nftTokenAddress_
-    ) ERC20(name_, symbol_) {
+        address nftTokenAddress_,
+        address vrfCoordinator,
+        address link,
+        bytes32 keyHash,
+        uint256 fee
+    )
+        public
+        ERC20(name_, symbol_)
+        VRFConsumerBase(vrfCoordinator, link)
+    {
         _mint(bagHolder, maxSupply);
-        nftTokenAddress = nftTokenAddress_;
+        nftToken = NakamotOsERC721(nftTokenAddress_);
+        s_keyHash = keyHash;
+        s_fee = fee;
     }
 
-    modifier onlyNFT() {
-        require(_msgSender() == nftTokenAddress, "Caller must be the NFT");
-        _;
-    }
-
-    function burn(uint256 amount) external returns (bool) {
+    function burn(uint256 amount, uint256 userProvidedSeed) external returns (bool) {
         _burn(_msgSender(), amount);
-        nftClaims[_msgSender()] += amount;
         emit Burn(_msgSender(), amount);
+
+        uint256 nftSupply = nftToken.totalSupply();
+        if (nftSupply < MAX_NFT_SUPPLY) {
+            uint256 i = amount;
+            for (i; i > 0; i = i.sub(ONE)) {
+                bytes32 requestId = requestRandomness(s_keyHash, s_fee, userProvidedSeed);
+                burner[requestId] = _msgSender();
+                emit LotteryEntered(requestId, _msgSender());
+            }
+        }
+
         return true;
     }
 
-    function claimNFT(uint amount, address minter) external onlyNFT returns (bool) {
-        uint256 claimAmount = amount * DECIMAL_MULTIPLIER;
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        uint256 d100Value = randomness.mod(100);
+        if (d100Value < 21) {
+            nftToken.mintOne(burner[requestId]);
+        }
 
-        require(nftClaims[minter] >= claimAmount);
-        nftClaims[minter] -= claimAmount;
-        return true;
+        emit LotteryResult(requestId, burner[requestId], d100Value < 21);
     }
 }
--- contracts/NakamotOsERC721.sol
+++ contracts/NakamotOsERC721.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity 0.6.9;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NakamotOsERC20.sol";
 
-contract NakamotOsERC721 is ERC721Enumerable, Ownable {
+contract NakamotOsERC721 is ERC721, Ownable {
     NakamotOsERC20 public nakamotOsErc20;
 
     string public _tokenURI;
@@ -15,8 +15,13 @@ contract NakamotOsERC721 is ERC721Enumerable, Ownable {
         string memory name,
         string memory symbol,
         string memory tokenURI_
-    ) ERC721(name, symbol) {
+    ) public ERC721(name, symbol) {
         _tokenURI = tokenURI_;
+    }
+
+    modifier onlyNakamotOs() {
+        require(_msgSender() == address(nakamotOsErc20), "Caller must the NakamotOsERC20");
+        _;
     }
 
     function setERC20Address(address erc20Address) external onlyOwner {
@@ -27,11 +32,7 @@ contract NakamotOsERC721 is ERC721Enumerable, Ownable {
         return _tokenURI;
     }
 
-    function mint(uint amount) external {
-        require(nakamotOsErc20.claimNFT(amount, _msgSender()), "Unable to claim token.");
-
-        for (uint i = 0; i < amount; i++) {
-            _safeMint(_msgSender(), totalSupply());
-        }
+    function mintOne(address recipient) external onlyNakamotOs returns (bool) {
+        _safeMint(recipient, totalSupply());
     }
 }
