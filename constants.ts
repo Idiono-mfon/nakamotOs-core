4d32f7d3b657a71017f994d926c82619993238e5
Sam <sam.hatem17@gmail.com>
2021-04-15 22:45:55 -0400

    feat: add NFT uri

--- constants.ts
+++ constants.ts
@@ -1,8 +1,8 @@
 import { ethers } from "hardhat";
 
 export const DECIMALS_MULTIPLIER = ethers.BigNumber.from(10).pow(18);
-export const MAX_SUPPLY = DECIMALS_MULTIPLIER.mul(2100);
+export const MAX_SUPPLY = DECIMALS_MULTIPLIER.mul(500);
 export const NAME = "Nakamot-Os";
 export const SYMBOL = "BOX";
-export const NFT_URI = "https://arweave.net/blahblahblah";
+export const NFT_URI = "https://arweave.net/w8Vs3I-TmRIcZVNaSkxjVhtWc9ZJleCJeSRlpXEVdPM";
 export const MAX_NFT_SUPPLY = 210;
