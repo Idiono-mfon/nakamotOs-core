6f29d96fc6ac44f9a6778f31626a769ea327eafd
christian-miljkovic <christian.miljkovic@gmail.com>
2021-05-18 19:06:45 -0400

    fix: address comments (incomplete)

--- tasks/start-lottery.ts
+++ tasks/start-lottery.ts
@@ -1,34 +1,33 @@
 import { task } from "hardhat/config";
-import { networkConfig, getNetworkIdFromName } from "../helper-hardhat-config";
+import { networkConfig, getNetworkIdFromName } from "../config/networks";
 import { START_LOTTERY } from "./task-names";
 
 task(START_LOTTERY, "Starts the NakamotOs lottery")
     .addParam("contract", "The address of the contract that requires LINK")
-    .addOptionalParam("linkaddress", "Set the LINK token address")
+    .addOptionalParam("amount", "The amount of LINK you need to transfer")
     .setAction(async (taskArgs, hre) => {
         const { ethers } = hre;
-        const contractAddress = taskArgs.contract;
+        const { BigNumber } = ethers;
+        const { contract: contractAddress, amount } = taskArgs;
         const { network } = hre;
         const networkId = Number(getNetworkIdFromName(network.name));
         const networkParams = networkConfig[networkId];
-        const linkTokenAddress = networkParams.linkToken || taskArgs.linkaddress;
-        const LinkToken = await ethers.getContractFactory("LinkToken");
+        const linkTokenAddress = networkParams.linkToken as string;
+        const ercToken = await ethers.getContractFactory("NakamotOsERC20");
 
-        const amount = ethers.utils.hexlify(ethers.constants.WeiPerEther);
-        console.log(amount);
+        const decimalMultiplier = BigNumber.from(10).pow(18);
+        const transferAmount =
+            amount !== undefined ? BigNumber.from(amount).mul(decimalMultiplier) : ethers.constants.WeiPerEther;
 
         const accounts = await ethers.getSigners();
         const signer = accounts[0];
 
-        const linkTokenContract = new ethers.Contract(linkTokenAddress, LinkToken.interface, signer);
-        const result = await linkTokenContract
-            .transfer(contractAddress, amount)
-            .then((transaction: any) => {
-                console.log("Contract ", contractAddress, " funded with 1 LINK. Transaction Hash: ", transaction.hash);
-            })
-            .catch((err: any) => {
-                console.log("hello error");
-                console.log(err);
-            });
-        console.log("result", result);
+        const linkTokenContract = new ethers.Contract(linkTokenAddress, ercToken.interface, signer);
+        try {
+            const result = await linkTokenContract.transfer(contractAddress, 100);
+            console.log("result", result);
+        } catch (error) {
+            console.log("made it here");
+            console.log({ error });
+        }
     });
