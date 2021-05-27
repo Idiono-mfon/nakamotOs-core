980484d37ade1a25ae88d2090305b5584f9e9832
Sam <sam.hatem17@gmail.com>
2021-05-27 00:58:24 -0400

    feat: deployments

--- deploy/001_deploy_nakamotOs.ts
+++ deploy/001_deploy_nakamotOs.ts
@@ -1,33 +1,19 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { MAX_SUPPLY, NAME, SYMBOL, NFT_URI, MAX_NFT_SUPPLY } from "../constants";
+import { MAX_SUPPLY, NAME, SYMBOL, NFT_URI, MAX_NFT_SUPPLY, BLOCKS_TIL_LOTTO } from "../constants";
+import { networkConfig, getNetworkIdFromName } from "../config/networks";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deployments, getNamedAccounts, network } = hre;
 
-    let chainlinkKeyhash;
-    let chainlinkVrfCoordinator;
-    let chainlinkToken;
+    const networkId = Number(getNetworkIdFromName(network.name));
+    const networkParams = networkConfig[networkId];
 
-    switch (network.name) {
-        case "mainnet": {
-            chainlinkKeyhash = "0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445";
-            chainlinkVrfCoordinator = "0xf0d54349aDdcf704F77AE15b96510dEA15cb7952";
-            chainlinkToken = "0x514910771af9ca656af840dff83e8264ecf986ca";
-            break;
-        }
-        case "localhost":
-        case "kovan": {
-            chainlinkKeyhash = "0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4";
-            chainlinkVrfCoordinator = "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9";
-            chainlinkToken = "0xa36085f69e2889c224210f603d836748e7dc0088";
-            break;
-        }
-        default:
-            throw new Error(`not set up for network ${network.name}`);
-    }
+    const { fee, keyHash, vrfCoordinator, linkToken } = networkParams;
 
     const { deployer } = await getNamedAccounts();
+
+    const bagHolder = network.name === "mainnet" ? "0x6384F5369d601992309c3102ac7670c62D33c239" : deployer;
 
     const nft = await deployments.deploy("NakamotOsERC721", {
         from: deployer,
@@ -40,12 +26,14 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
             NAME,
             SYMBOL,
             MAX_SUPPLY,
-            deployer,
+            bagHolder,
             nft.address,
             MAX_NFT_SUPPLY,
-            chainlinkKeyhash,
-            chainlinkVrfCoordinator,
-            chainlinkToken,
+            BLOCKS_TIL_LOTTO,
+            keyHash,
+            vrfCoordinator,
+            linkToken,
+            fee,
         ],
         log: true,
     });
